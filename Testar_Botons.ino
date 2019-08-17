dd8d3602e463b30f871d050396cf3f8d9a28fc0c
André <40214038+AndreSalgueiro@users.noreply.github.com>
2019-08-17 16:23:58 -0300

    Testar Botão

---
+++ Testar_Botons.ino
@@ -0,0 +1,32 @@
+#define LED_PIN 13
+#define BUT_ACEL_PIN 2
+#define BUT_DESAC_PIN 4
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUT_ACEL_PIN, INPUT);
+   pinMode(BUT_DESAC_PIN, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int butonAcel = digitalRead(BUT_ACEL_PIN);
+  int butonDesac = digitalRead(BUT_DESAC_PIN);
+  
+  if(!butonAcel){
+    digitalWrite(LED_PIN, HIGH);
+    }
+    if(butonAcel){
+      
+    digitalWrite(LED_PIN, LOW);
+    }
+    
+  if(!butonDesac){
+    digitalWrite(LED_PIN, HIGH);
+    }
+  if(butonDesac){
+    digitalWrite(LED_PIN, LOW);
+    }
+  
+}
