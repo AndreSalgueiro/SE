4a4a7d87c067a61b4d51b352b0a5022419af7bc5
André <40214038+AndreSalgueiro@users.noreply.github.com>
2019-08-31 15:56:01 -0300

    Add files via upload

---
+++ Testar_alarme_Piezzo.ino
@@ -0,0 +1,18 @@
+int i =0;
+void setup() {
+Serial.begin(9600);
+pinMode(5,OUTPUT);
+
+}
+
+void loop() {
+for (i=0;i<25;i++){
+analogWrite(5,i);
+delay(5);
+}
+/*for (i=255;i>-1;i--){
+analogWrite(5,i);
+delay(5);
+}*/
+
+}
